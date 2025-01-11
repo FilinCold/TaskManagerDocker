181306d54cddee9a4e4db20217503ba611fb2a42
Vladislav Proskurkin <proskurkin-va@zvuk.com>
2025-01-11 20:52:33 +0300

    log every 30 min

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ app.listen(port, async () => {
   await sheet.loadCells("A1:K10");
 
   // For test========== every run task 5 secs
-  cron.schedule("*/5 * * * * *", async () => {
-    console.log("running a task every 30 secs for test");
+  cron.schedule("30 * * * *", async () => {
+    console.log("running a task every 30 min for test");
   });
 
   // // проверяет предыдущие записанные матчи изменяет бюджет и удаляет их из таблицы
